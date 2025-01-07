d8000e47ac20c2fb95e865b5c9e445db76227c5a
svvkcn <10385951@qq.com>
2025-01-08 00:08:23 +0800

    11

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,2 +1,13 @@
-export * from './nav'
-export * from './sidebar'
+import type { DefaultTheme } from 'vitepress'
+
+const nav: DefaultTheme.Config['nav'] = [
+    { text: '首页', link: '/' },
+    { text: '导航', link: '/navigation/index' }
+]
+
+const sidebar: DefaultTheme.Config['sidebar'] = {}
+
+export default {
+    nav,
+    sidebar
+}
--- docs/.vitepress/config/nav.ts
+++
@@ -1,6 +0,0 @@
-import type { DefaultTheme } from 'vitepress'
-
-export const nav: DefaultTheme.Config['nav'] = [
-  { text: '首页', link: '/' },
-  { text: '导航', link: '/navigation/index' }
-]
